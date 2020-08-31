bc0b7500735e81de31cbde351317bc5b8fdcfc5f
David Ruano <dargruano@gmail.com>
2020-08-31 10:54:18 -0500

    const and let

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+const nombre = 'David';
+const apellido = 'Ruano';
+
+let valorDado = 5;
+//let valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+
+    const nombre = "Alejandro";
+    console.log(nombre);
+}
+console.log(nombre);
